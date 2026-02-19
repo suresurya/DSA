d950e555a64c52eb7a4db8df4967c5705af95b68
suresurya <suresrivenkatramasurya@gmail.com>
2026-02-19 17:03:00 +0530

    daf

--- Main.java
+++ Main.java
@@ -1,9 +1,13 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
+    IO.print("Hello World!!!! ");
+    IO.print("Hello World!!!! ");
+    IO.print("Hello World!!!! ");
+    IO.print("Hello World!!!! ");
+    IO.print("Hello World!!!! ");
+    IO.print("Hello World!!!! ");
+    IO.print("Hello World!!!! ");
 
     for (int i = 1; i <= 5; i++) {
         //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
