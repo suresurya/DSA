a82c6554044c7333bc68600056dd14328b866a75
suresurya <suresrivenkatramasurya@gmail.com>
2026-04-01 09:32:59 +0530

    1-4-26 basic input and ouput

--- Basic_Input_Output.java
+++ Basic_Input_Output.java
@@ -1,3 +1,10 @@
-public class Basic_Input_Output {
+import java.util.*;
 
+public class Basic_Input_Output {
+    static void main() {
+        Scanner sc=new Scanner(System.in);
+        int n =sc.nextInt();
+        System.out.println(n);
+
+    }
 }
