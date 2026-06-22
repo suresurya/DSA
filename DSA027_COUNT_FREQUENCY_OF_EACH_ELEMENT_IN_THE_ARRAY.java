a6905c0c9d9861edffbf93c2786c47d24bdea7c6
suresurya <suresrivenkatramasurya@gmail.com>
2026-06-22 23:13:01 +0530

    Hashing

---
+++ DSA027_COUNT_FREQUENCY_OF_EACH_ELEMENT_IN_THE_ARRAY.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class DSA027_COUNT_FREQUENCY_OF_EACH_ELEMENT_IN_THE_ARRAY {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        
+
+        sc.close();
+    }
+}
