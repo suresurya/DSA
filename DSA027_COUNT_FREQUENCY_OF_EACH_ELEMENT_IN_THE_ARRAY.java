6adf1110180d92df6be63c53238a5ac4cb4a0141
suresurya <suresrivenkatramasurya@gmail.com>
2026-06-24 16:40:22 +0530

    Dangerous

--- DSA027_COUNT_FREQUENCY_OF_EACH_ELEMENT_IN_THE_ARRAY.java
+++ DSA027_COUNT_FREQUENCY_OF_EACH_ELEMENT_IN_THE_ARRAY.java
@@ -3,10 +3,19 @@ import java.util.*;
 public class DSA027_COUNT_FREQUENCY_OF_EACH_ELEMENT_IN_THE_ARRAY {
 
     public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String s=sc.nextLine();
+        int [] hashmap=new int[26];
+        int n=hashmap.length;
+
+        for(int i =0;i<s.length();i++){
+        hashmap[s.charAt(i)-'a']++;
+
+            System.out.println(hashmap[s.charAt(i)-'a']);
+        }
 
 
-        
 
-        sc.close();
+                sc.close();
     }
 }
