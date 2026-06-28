869842d285cb5fd65b3f872c3865ad08327221ab
suresurya <suresrivenkatramasurya@gmail.com>
2026-06-28 20:25:49 +0530

    Nothing to know more

--- DSA038_FREQUENCY_COUNT_BUT_DIFFENT_OUTPUTFORMAT.java
+++ DSA038_FREQUENCY_COUNT_BUT_DIFFENT_OUTPUTFORMAT.java
@@ -5,8 +5,16 @@ public class DSA038_FREQUENCY_COUNT_BUT_DIFFENT_OUTPUTFORMAT {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
+        String s=sc.next().toLowerCase();
+        Map<Character,Integer> map=new LinkedHashMap<>();
+        for(char c : s.toCharArray())
+            map.put(c,map.getOrDefault(c,0)+1);
 
-        
+        String ans=map.toString()
+                .replace("{","{'")
+                        .replace(",","',").replace("=",":")
+                        .replace(":","':");
+        System.out.println(ans);
 
         sc.close();
     }
